50cf9a81e4d797707fd0a5be55dc36d132c365c5
rushabhjchauhan99 <46366301+rushabhjchauhan99@users.noreply.github.com>
2019-01-06 23:45:52 -0500

    Create simpleoutputs.java

---
+++ lab0part1/src/lab0part1/simpleoutputs.java
@@ -0,0 +1,11 @@
+package lab0part1;
+
+public class simpleoutputs 
+{
+	public static void main (String args [])
+	{
+		System.out.println("hellow EECS 1021");
+		System.out.println("this is winter 2019");
+	}
+	}
+	
